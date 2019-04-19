4a63f7b0fc9d729f77a77e9d4bc498db09852409
JanggoSmart <49754997+JanggoSmart@users.noreply.github.com>
2019-04-19 08:32:30 +0700

    Add files via upload

---
+++ Tools1.php
@@ -0,0 +1,14 @@
+<?php
+$shell = "site.com/shell.php";
+$cek_header = get_headers($shell);
+
+	if(preg_match('/200/', $cek_header[0})) {
+	
+		echo "Shell Ketemu Mhamank";
+		
+		} else {
+		
+		echo "Shell Gaketemu :(";
+		
+		}
+?>
